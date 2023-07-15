0ec96090ffe028def58b333900108148c29e4019
dev1week <rondo2860@gmail.com>
2023-07-15 18:42:52 +0900

    #1 -2 Vue.js로 구현하기

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,13 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
-
-function addGoal() {
-  const enteredValue = input.value;
-  const linstItemEl = document.createElement("li");
-  linstItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = "";
-}
-
-button.addEventListener("click", addGoal);
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+    },
+  },
+}).mount("#app");
